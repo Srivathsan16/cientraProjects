f07bbb19cd76243fa575614921634102a54415bd
Srivathsan <srivathsanrajagopal@gmail.com>
2018-09-09 11:34:06 +0530

    initial commit

---
+++ src/main/java/com/cientra/solutions/cientraProjects/CientraProjectsApplication.java
@@ -0,0 +1,18 @@
+package com.cientra.solutions.cientraProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class CientraProjectsApplication {
+
+	public static void main(String[] args) {
+		run(CientraProjectsApplication.class,args);
+
+		/*SpringApplication.run(CientraProjectsApplication.class, args);*/
+	}
+}
---
+++ src/test/java/com/cientra/solutions/cientraProjects/CientraProjectsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cientra.solutions.cientraProjects;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CientraProjectsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
